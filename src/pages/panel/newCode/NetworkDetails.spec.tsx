5c6fec35a4de0396e848c6b8bd941d871f88a7cc
Ghis <ghisloufer@hotmail.fr>
2022-11-14 23:44:46 +0100

    wip test

---
+++ src/pages/panel/newCode/NetworkDetails.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import { NetworkDetails } from "./NetworkDetails";
+
+describe("NetworkDetails component", () => {
+  it("should render", async () => {
+    // ARRANGE
+    render(<NetworkDetails />);
+
+    // ACT
+    await userEvent.click(screen.getByText("Load Greeting"));
+    await screen.findByRole("heading");
+
+    // ASSERT
+    expect(screen.getByRole("heading")).toHaveTextContent("hello there");
+    expect(screen.getByRole("button")).toBeDisabled();
+  });
+});
